d859010578312dd2730904b6e9b7e27880dc7409
Orcoholist <orcoholist@mail.ru>
2025-08-26 01:46:33 +0300

    adress

--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -22,7 +22,7 @@ export default function RegisterPage() {
     }
 
     try {
-      const res = await fetch('time-warp-back.vercel.app/auth/register', {
+      const res = await fetch('https://time-warp-back.vercel.app/auth/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
